5f6c05586471598f11413f1c64052f008006fb53
megadose <51822495+megadose@users.noreply.github.com>
2020-08-07 14:50:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='holehe',
-    version="1.024",
+    version="1.3",
     packages=find_packages(),
     author="megadose",
     install_requires=["requests","evolut","argparse","termcolor","tqdm", "mechanize", "fake_useragent"],
